d15f30cf64183faab28de8fd2e60d214ff31e8e1
Subhranil Mukherjee <iamsubhranil@users.noreply.github.com>
2019-08-26 13:51:16 +0530

    added initial graph programs

---
+++ 1b.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+	int mat[100][100];
+	int v, e,a , b, w;
+	printf("Enter number of vertices : ");
+	scanf("%d", &v);
+	printf("enter the number of edges : ");
+	scanf("%d", &e);
+	if(v >= 100) {
+		printf("Number of vertices is out of range!");
+		return 1;
+	}
+	
+	for(int i = 0; i < e; i++) {
+		printf("Enter the terminal vertices for edge %d : ", i + 1);
+		scanf("%d%d", &a, &b);
+		if(a > v || b > v || a < 1 || b < 1){
+			printf("Invalid edge : (%d, %d)\n", a, b);
+			i--;
+			continue;
+		}
+		printf("Enter the weight for edge %d : ", i + 1);
+		scanf("%d", &w);
+		a--;
+		b--;
+		mat[a][b] = mat[b][a] = w;
+	}
+	
+	printf("The adjacency matrix : \n\t");
+	for(int i = 0; i < v; i++) {
+		printf("v%2d\t", i + 1);
+	}
+	printf("\n");
+	for(int i = 0;i < v;i++){
+		printf("v%2d\t", i + 1);
+		for(int j = 0; j < v;j++) {
+			printf("%2d\t", mat[i][j]);
+		}
+		printf("\n\n");
+	}
+	printf("\n");
+	return 0;
+}
---
+++ 3b.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+	int vertex;
+	int weight;
+	struct Node *next;
+} Node;
+
+void insertNodeInto(Node **head, Node *toInsert) {
+	if(*head == NULL) {
+		*head = toInsert;
+		return;
+	}
+	Node *trav = *head;
+	while(trav->next != NULL) {
+		if(trav->vertex == toInsert->vertex) {
+			free(toInsert);	
+			return;
+		}
+		trav = trav->next;
+	}
+	
+	trav->next = toInsert;
+}
+
+Node* new_node(int v, int weight) {
+	Node *n = (Node*)malloc(sizeof(Node));
+	n->next = NULL;
+	n->vertex = v;
+	n->weight = weight;
+	return n;
+}
+
+int main() {
+	Node *array[100] = {NULL};
+	
+	int v, e, a, b, w;
+	printf("Enter number of vertices : ");
+	scanf("%d", &v);
+	if(v >= 100) {
+		printf("Number of vertices out of range!\n");
+		return 0;
+	}
+	printf("Enter number of edges : ");
+	scanf("%d", &e);
+	
+	for(int i = 0; i < e;i++) {
+		printf("Enter terminal vertices for edge %d : ", i + 1);
+		scanf("%d%d", &a, &b);
+		if(a > v || b > v || a < 1 || b < 1) {
+			printf("Invalid edge (%d, %d)!\n", a, b);
+			continue;
+		}
+		printf("Enter weight for edge %d : ", i + 1);
+		scanf("%d", &w);
+		a--;
+		b--;
+		Node *v1 = new_node(a + 1, w);
+		Node *v2 = new_node(b + 1, w);
+		insertNodeInto(&array[a], v2);
+		insertNodeInto(&array[b], v1);
+	}
+	
+	printf("Adjacency list : \n");
+	for(int i = 0; i < v; i++){
+		printf("V%d ", i + 1);
+		Node *head = array[i];
+		while(head != NULL) {
+			printf("--> V%d(%d) ", head->vertex, head->weight);
+			head = head->next;
+		}
+		printf("--> X\n");
+	}
+}
