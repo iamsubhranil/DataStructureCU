44b93498e4a5db0edc0dcadfb5e432439510c452
Subhranil Mukherjee <iamsubhranil@users.noreply.github.com>
2019-08-27 16:37:38 +0530

    1a : take vertex names explicitly

--- 1a.cpp
+++ 1a.cpp
@@ -1,39 +1,75 @@
 #include <stdio.h>
+#include <string.h>
+
+int findVertexNum(char vertexMap[][100], char *toFind) {
+	for(int i = 0; i < 100; i++){
+		if(strcmp(vertexMap[i], toFind) == 0)
+			return i + 1;
+	}
+	return -1;
+}
+
+int findMaxLength(char vertexMap[][100], int vertices){
+	int len = 0, temp = 0;
+	for(int i = 0;i < vertices;i++){
+		if((temp = strlen(vertexMap[i]) > len))
+			len = temp;
+	}
+	return len;
+}
+
+int numberOfDigits(int n) {
+	int d = 1;
+	while(n > 10){
+		n /= 10;
+		d++;
+	}
+	return d;
+}
 
 int main() {
 	int mat[100][100];
-	int v, e,a , b;
+	char vertexMap[100][100];
+	int v, e;
+	char a[100] , b[100];
 	printf("Enter number of vertices : ");
 	scanf("%d", &v);
-	printf("enter the number of edges : ");
-	scanf("%d", &e);
 	if(v >= 100) {
 		printf("Number of vertices is out of range!");
 		return 1;
 	}
+	printf("Enter the name of the vertices : ");
+	for(int i = 0;i < v;i++){
+		printf("\nV%d : ", i + 1);
+		scanf("%s", vertexMap[i]);
+	}
+	printf("Enter the number of edges : ");
+	scanf("%d", &e);
 	
 	for(int i = 0; i < e; i++) {
 		printf("Enter the terminal vertices for edge %d : ", i + 1);
-		scanf("%d%d", &a, &b);
-		if(a > v || b > v || a < 1 || b < 1){
-			printf("Invalid edge : (%d, %d)\n", a, b);
+		scanf("%s%s", a, b);
+		int aNum = findVertexNum(vertexMap, a);
+		int bNum = findVertexNum(vertexMap, b);
+		if(aNum > v || bNum > v || aNum < 1 || bNum < 1){
+			printf("Invalid edge : (%s, %s)\n", a, b);
 			i--;
 			continue;
 		}
-		a--;
-		b--;
-		mat[a][b] = mat[b][a] = 1;
+		aNum--;
+		bNum--;
+		mat[aNum][bNum] = mat[bNum][aNum] = 1;
 	}
-	
+	int width = findMaxLength(vertexMap, v);
 	printf("The adjacency matrix : \n\t");
 	for(int i = 0; i < v; i++) {
-		printf("v%2d\t", i + 1);
+		printf("%*s\t", width, vertexMap[i]);
 	}
 	printf("\n");
 	for(int i = 0;i < v;i++){
-		printf("v%2d\t", i + 1);
+		printf("%*s\t", width, vertexMap[i]);
 		for(int j = 0; j < v;j++) {
-			printf("%2d\t", mat[i][j]);
+			printf("%*d\t", width, mat[i][j]);
 		}
 		printf("\n\n");
 	}
