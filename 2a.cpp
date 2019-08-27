205939517e0da31d438b50bf00ec25a1d226304e
Subhranil Mukherjee <iamsubhranil@users.noreply.github.com>
2019-08-27 16:38:44 +0530

    2a: incorporate graph reader

--- 2a.cpp
+++ 2a.cpp
@@ -1,25 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+typedef struct Graph {
+	int numOfVertices;
+	int numOfEdges;
+	int **edges;
+} Graph;
+
+void generateIncidenceMatrix(Graph *g) {
 	int mat[100][100];
-	int v, e,a , b;
-	printf("Enter number of vertices : ");
-	scanf("%d", &v);
-	printf("enter the number of edges : ");
-	scanf("%d", &e);
-	if(v >= 100 || e >= 100 || v < 0 || e < 0) {
-		printf("Number of vertices or number of edges is out of range!");
-		return 1;
-	}
 	
-	for(int i = 0; i < e; i++) {
-		printf("Enter the terminal vertices for edge %d : ", i + 1);
-		scanf("%d%d", &a, &b);
-		if(a > v || b > v || a < 1 || b < 1){
-			printf("Invalid edge : (%d, %d)\n", a, b);
-			i--;
-			continue;
-		}
+	for(int i = 0; i < g->numOfEdges; i++) {
+		int a = g->edges[i][0], b = g->edges[i][1];
 		a--;
 		b--;
 		mat[a][i] = 1;
@@ -27,17 +19,72 @@ int main() {
 	}
 	
 	printf("The incidence matrix : \n\t");
-	for(int i = 0; i < e; i++) {
+	for(int i = 0; i < g->numOfEdges; i++) {
 		printf("e%2d\t", i + 1);
 	}
 	printf("\n");
-	for(int i = 0;i < v;i++){
+	for(int i = 0;i < g->numOfVertices;i++){
 		printf("v%2d\t", i + 1);
-		for(int j = 0; j < e;j++) {
+		for(int j = 0; j < g->numOfEdges;j++) {
 			printf("%2d\t", mat[i][j]);
 		}
 		printf("\n\n");
 	}
 	printf("\n");
+}
+
+void inputGraph(Graph *g) {
+	printf("Enter number of vertices : ");
+	scanf("%d", &g->numOfVertices);
+	printf("Enter the number of edges : ");
+	scanf("%d", &g->numOfEdges);
+	g->edges = (int **)malloc(sizeof(int*) * g->numOfEdges);
+	for(int i = 0; i < g->numOfEdges; i++)
+		g->edges[i] = NULL;
+	for(int i = 0; i < g->numOfEdges; i++) {
+		printf("Enter the terminal vertices for edge %d : ", i + 1);
+		if(!g->edges[i])
+			g->edges[i] = (int*)malloc(sizeof(int) * 2);
+		scanf("%d%d", &(g->edges[i][0]), &(g->edges[i][1]));
+		if(g->edges[i][0] > g->numOfVertices || g->edges[i][1] > g->numOfVertices 
+		|| g->edges[i][0] < 1 || g->edges[i][1] < 1){
+			printf("Invalid edge : (%d, %d)\n", g->edges[i][0], g->edges[i][1]);
+			i--;
+			continue;
+		}
+	}
+}
+
+void readGraph(Graph *g, const char *file) {
+	FILE *f = fopen(file, "r");
+	if(f == NULL) {
+		printf("Error: Unable to read '%s'!\n", file);
+		return;
+	}
+	fscanf(f, "%d", &g->numOfVertices);
+	fscanf(f, "%d", &g->numOfEdges);
+	g->edges = (int **)malloc(sizeof(int*) * g->numOfEdges);
+	for(int i = 0; i < g->numOfEdges; i++) {
+		g->edges[i] = (int*)malloc(sizeof(int) * 2);
+		fscanf(f, "%d%d", &(g->edges[i][0]), &(g->edges[i][1]));
+	}
+	fclose(f);
+}
+
+
+void printGraph(Graph *g) {
+	printf("Number of vertices : %d\n", g->numOfVertices);
+	printf("Number of edges : %d\n", g->numOfEdges);
+	for(int i = 0; i < g->numOfEdges; i++) {
+		printf("Edge %d : (%d, %d)\n", i + 1, g->edges[i][0], g->edges[i][1]);
+	}
+}
+
+int main() {
+	Graph g;
+	inputGraph(&g);
+	//readGraph(&g, "graphinput.txt");
+	//printGraph(&g);
+	generateIncidenceMatrix(&g);
 	return 0;
 }
